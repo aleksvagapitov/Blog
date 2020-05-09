f00449b1f0207f5fe7e4fd07563f5d05292dfc53
Aleksandr <aleksvagapitov@gmail.com>
2020-05-09 18:19:45 -0400

    Changed Deployment Script

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,8 @@ mkdir public
 git worktree prune
 rm -rf .git/worktrees/public/
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
+echo "Checking out master branch into public"
+git worktree add -B master public origin/master
 
 echo "Removing existing files"
 rm -rf public/*
@@ -22,8 +22,14 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing"
+# Adding Commit Message
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+
+echo "Updating master branch"
+cd public && git add --all && git commit -m "$msg"
 
 #echo "Pushing to github"
-git push origin gh-pages
+git push origin master
